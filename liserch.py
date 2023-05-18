485532d1e16dfc94411ae84f26239e867c8ee5b7
Harikrishna mundra <123417887+hari3399@users.noreply.github.com>
2023-05-18 01:22:58 -0500

    Update liserch.py

--- liserch.py
+++ liserch.py
@@ -1,16 +1,21 @@
-#program to implement linear serch algoritham in python
-def liserch(list,key,len):
-    counter = 0
-    for i in range(0,len):
-         counter += 1
-         if (list[i] == key):
-             return key,counter
-    return ("not in list")
-if __name__== "__main__":
-      list = list(range(1,101))
-      len = len(list)
-      key = int(input("please enter number which you want to serch:  "))
+# implementaion of linearsearch algoritham using python for basic understanding
 
-result = liserch(list,key,len)
-print(result)
-  
+def liserch(list, key, len):
+    counter = 0  # Initialize a counter to keep track of the number of iterations
+
+    for i in range(0, len):  # Iterate through each element in the list
+        counter += 1  # Increment the counter
+
+        if list[i] == key:  # Check if the current element matches the key
+            return key, counter  # Return the key and the number of iterations taken
+
+    return ("not in list")  # Return a message indicating that the key is not found in the list
+
+
+if __name__ == "__main__":
+    list = list(range(1, 101))  # Create a list from 1 to 100
+    len = len(list)  # Get the length of the list
+    key = int(input("please enter number which you want to search: "))  # Get the key from user input
+
+result = liserch(list, key, len)  # Call the linear search function
+print(result)  # Print the result to the console
